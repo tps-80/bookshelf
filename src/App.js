306b0d3a27332f417d039ff0ca70b7d0b502078f
Thomas Sorensen <thomaspsorensen@gmail.com>
2017-09-05 15:25:02 -0700

    feat: Change div to BookTop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Select from './Select'
+import BookTop from './BookTop'
 
 class BooksApp extends React.Component {
   state = {
@@ -113,12 +114,7 @@ class BooksApp extends React.Component {
                     <ol className="books-grid">
                       <li>
                         <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
-                            <div className="book-shelf-changer">
-                              <Select />
-                            </div>
-                          </div>
+                          <BookTop coverStyle={{width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")'}}/>
                           <div className="book-title">The Hobbit</div>
                           <div className="book-authors">J.R.R. Tolkien</div>
                         </div>
