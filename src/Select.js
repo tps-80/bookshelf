0613fb59acf7623ed6a38270818e907f1e067b2e
Thomas Sorensen <thomaspsorensen@gmail.com>
2017-09-05 13:51:34 -0700

    fix: Add enclosing div

--- src/Select.js
+++ src/Select.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 class Select extends Component {
     render() {
         return (
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
+            <div>
+                <option value="none" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </div>
         )
     }
 }
