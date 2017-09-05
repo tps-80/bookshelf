cbc05f9077a942bd10721a6a587098aa71087748
Thomas Sorensen <thomaspsorensen@gmail.com>
2017-09-05 14:51:11 -0700

    feat: Create BookTop component

---
+++ src/BookTop.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Select from './Select'
+
+class BookTop extends Component {
+    render() {
+        return (
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
+                <div className="book-shelf-changer">
+                    <Select />
+                </div>
+            </div>
+        ) 
+    }
+}
+
+export default BookTop
