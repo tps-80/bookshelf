e458bdd3512ce6fb9abadc0bb6d0574da7a67b10
Thomas Sorensen <thomaspsorensen@gmail.com>
2017-09-05 13:45:40 -0700

    feat: Create select component

---
+++ src/Select.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Select extends Component {
+    render() {
+        return (
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+        )
+    }
+}
+
+
+export default Select
